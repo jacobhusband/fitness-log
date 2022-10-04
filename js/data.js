d66f766cfadd806216376bc6204a7a21761c801d
Jacob Husband <jakehusband2@gmail.com>
2022-10-04 01:00:36 +0000

    Fix localstorage merging data from other apps

--- js/data.js
+++ js/data.js
@@ -7,14 +7,24 @@ window.addEventListener('pagehide', function (event) {
   localStorage.setItem('data', JSON.stringify(data));
 });
 
-var data = JSON.parse(localStorage.getItem('data'));
+var grabbedData = JSON.parse(localStorage.getItem('data'));
+var badData = false;
 
-if (!data || !data.view || !data.nextExerciseId || !data.organizedExercises) {
-  data = {
-    view: 'upcoming-workouts',
-    exercises: [],
-    nextExerciseId: 1,
-    organizedExercises: [],
-    desktopCurrentDayView: 0
-  };
+var data = {
+  view: 'upcoming-workouts',
+  exercises: [],
+  nextExerciseId: 1,
+  organizedExercises: [],
+  desktopCurrentDayView: 0
+};
+
+for (var key in grabbedData) {
+  if (data[key] === undefined || Object.keys(data).length !== Object.keys(grabbedData).length) {
+    badData = true;
+    break;
+  }
+}
+
+if (!badData) {
+  data = grabbedData;
 }
